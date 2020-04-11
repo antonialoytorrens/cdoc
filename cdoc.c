fd1a0cf5399ae387b6535bdfc3da6d434d3d011a
ashn <me@ashn.dev>
2020-04-11 14:46:39 -0400

    Add cdoc documentation for constructs in cdoc.c

--- cdoc.c
+++ cdoc.c
@@ -1,3 +1,6 @@
+//! @file cdoc.c
+//! @license 0BSD
+
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -6,43 +9,193 @@
 
 #define VERSION "0.1"
 
+//! @function usage
+//!     Print usage information and exit.
 static void
 usage(void);
-
+//! @function version
+//!     Print version information and exit.
 static void
 version(void);
 
+//! @macro LINENO
+//!     Calculates a 1-indexed line number from the pointer off set of two
+//!     pointers to lines in the file.
+//! @param start
+//!     Starting line of the file.
+//! @param cur
+//!     Some line in the file.
+#define LINENO(/* char** */ start, /* char** */ cur) ((int)(cur - start + 1))
+
+//! @function errorf
+//!     Write a formatted error message and exit with EXIT_FAILURE status.
+//! @param fmt
+//!     Printf-style format string.
+//! @param ...
+//!     Format arguments.
+//! @note
+//!     A newline character is automatically written after the formatted error
+//!     message.
+static void
+errorf(char const* fmt, ...);
+//! @function xalloc
+//!     General purpose allocation & reallocation function that will never
+//!     return an invalid pointer.
+//!     Behavior is identical to realloc with the following exceptions:
+//!     (1) Allocation failure will print an error message and terminate the
+//!         program with EXIT_FAILURE status.
+//!     (2) The call xalloc(p, 0) is guaranteed to free the memory backing p.
+//!     A pointer p returned by xalloc may be freed with xalloc(p, 0) or
+//!     free(p).
+static void*
+xalloc(void* ptr, size_t size);
+//! @function read_text_file
+//!     Returns the contents of stream as a NUL-terminated heap-allocated
+//!     cstring.
+//!     Encountering a NUL-byte or read failure will print an error message and
+//!     terminate the program with EXIT_FAILURE status.
+static char*
+read_text_file(FILE* stream);
+//! @function text_to_lines
+//!     Transform provided NUL-terminated text string into a heap-allocated
+//!     NULL-terminated list of NUL-terminated cstrings by replacing newline
+//!     characters with NUL bytes.
+//! @note
+//!     This function modifies text in place.
+static char**
+text_to_lines(char* text);
+//! @function is_hspace
+//!     Returns true if c is a horizontal whitespace character.
+//!     The characters '\t', '\r', and ' ' are considered horizontal whitespace.
+static bool
+is_hspace(int c);
+//! @function is_doc_line
+//!     Returns true the first non-whitespace characters of line are "//!", the
+//!     start of a doc-line in cdoc.
+//! @param line
+//!     NUL-terminated character string.
+static bool
+is_doc_line(char const* line);
+//! @function doc_content_start
+//!     Returns a pointer to the non whitespace character after the initial
+//!     "//!" in a doc-line.
+//!     Terminates the program with EXIT_FAILURE status if line does not
+//!     satisfy the definition of a doc-line as defined by is_doc_line.
+static char const*
+doc_content_start(char const* line);
+
+//! @function do_file
+//!     Generate documentation for the provided file.
+//! @param fp
+//!     File pointer returned from fopen.
+//!     The position fp's file offset should be at the beginning of the file.
 static void
 do_file(FILE* fp);
 
-#define LINENO(/* char** */ start, /* char** */ cur) ((int)(cur - start + 1))
-
+//! @struct doc
+//!     Representation of a document, aka a list of doc-sections documenting the
+//!     a single portion of a C source file.
+//!
+//!     The cdoc documentation:
+//!     <pre><code>
+//!     //! @struct foo
+//!     //!     The struct used for fooing.
+//!     //! @note
+//!     //!     Be careful not to bar this foo.
+//!     struct foo
+//!     {
+//!         int some_member;
+//!     };
+//!     </code></pre>
+//!     is comprised of two sections (beginning with @variable and @note) and
+//!     four lines of source.
 struct doc
 {
-    struct section* sections; // dynamically allocated
+    //! @member sections
+    //!     Dynamically allocated list of sections in this doc.
+    struct section* sections;
+    //! @member section_count
+    //!     Number of sections in this doc.
     size_t section_count;
-
-    // NULL-terminated list of associated source code.
-    char** source; // dynamically allocated
+    //! @member source
+    //!     Dynamically allocated NULL-terminated list of source code associated
+    //!     with this doc.
+    //!     A value of NULL implies that this doc has no associated source code.
+    char** source;
 };
-static struct doc
-parse_doc(char* const* lines_begin, char*** linepp);
-static void
-print_doc(struct doc const d);
-
+//! @struct section
+//!     Representation of a portion of a document, specifically one facet of the
+//!     subject of the doc.
+//!     Each section has a mandatory tag, an optional name, and optional body
+//!     of description text.
+//!     The doc-lines:
+//!     <pre><code>
+//!     //! @struct foo
+//!     //!     The struct used for fooing.
+//!     </code></pre>
+//!     form one section with the tag "struct", the name "foo" and a text body
+//!     containing the single line of text "The struct used for fooing.".
 struct section
 {
+    //! @member tag_start
+    //!     Location of the first character in the tag slice.
     char const* tag_start;
+    //! @member tag_len
+    //!     Length of the tag slice.
     int tag_len;
 
+    //! @member name_start
+    //!     Location of the first character in the name slice.
     char const* name_start;
-    int name_len; // name_len == 0 implies tag line has no name
+    //! @member name_len
+    //!     Length of the name slice.
+    //!     A length of zero implies that this tag has no name.
+    int name_len;
 
+    //! @member text_start
+    //!     Location of the first line in the text body.
     char** text_start;
+    //! @member text_len
+    //!     Length of the text body.
+    //!     A length of zero implies that this tag has no body.
     int text_len;
 };
+
+//! @function parse_struct_source
+//!     Parse the lines of a struct forward declaration or definition using the
+//!     provided parse state.
+//! @note
+//!     The method used for parsing in this function happens to works for
+//!     struct, union, enum, typedef, and variable declarations.
+//! @todo
+//!     Currently this function does not handle comments (C or C++ style)
+//!     correctly which may lead to erroneous parsing.
+//!     This function should be modified to correctly handle comments.
+static char**
+parse_struct_source(char*** linepp);
+//! @function parse_function_source
+//!     Parse the lines of a function prototype or definition using the provided
+//!     parse state.
+static char**
+parse_function_source(char*** linepp);
+//! @function parse_macro_source
+//!     Parse the lines of a preprocessor macro using the provided parse state.
+static char**
+parse_macro_source(char*** linepp);
+//! @function parse_doc
+//!     Construct and return a doc from the provided parse state parameters.
+static struct doc
+parse_doc(char* const* lines_begin, char*** linepp);
+//! @function print_doc
+//!     Print a formatted representation of this doc to stdout.
+static void
+print_doc(struct doc const d);
+//! @function parse_section
+//!     Construct and return a section from the provided parse state parameters.
 static struct section
 parse_section(char* const* lines_begin, char*** linepp);
+//! @function print_section
+//!     Print a formatted representation of this section stdout.
 static void
 print_section(struct section const s);
 
@@ -128,7 +281,6 @@ xalloc(void* ptr, size_t size)
     return ptr;
 }
 
-// Returns the contents of stream as a NUL-terminated heap-allocated cstring.
 static char*
 read_text_file(FILE* stream)
 {
@@ -149,9 +301,6 @@ read_text_file(FILE* stream)
     return buf;
 }
 
-// Transform the NUL-terminated text into a heap-allocated NULL-ended list of
-// NUL-terminated cstrings by replacing newline characters with NUL bytes.
-// Modifies text in place.
 static char**
 text_to_lines(char* text)
 {
