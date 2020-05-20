df70bd46e51c2c7512880fc50698d9535ff05d73
ashn <me@ashn.dev>
2020-05-19 22:40:48 -0400

    Add braces around all control statements

--- cdoc.c
+++ cdoc.c
@@ -205,10 +205,12 @@ main(int argc, char** argv)
     bool parse_options = true;
     for (int i = 1; i < argc; ++i) {
         char const* const arg = argv[i];
-        if (parse_options && strcmp(arg, "--help") == 0)
+        if (parse_options && strcmp(arg, "--help") == 0) {
             usage();
-        if (parse_options && strcmp(arg, "--version") == 0)
+        }
+        if (parse_options && strcmp(arg, "--version") == 0) {
             version();
+        }
         if (parse_options && strcmp(arg, "--") == 0) {
             parse_options = false;
             continue;
@@ -274,10 +276,12 @@ terminate:
 static void*
 xalloc(void* ptr, size_t size)
 {
-    if (size == 0)
+    if (size == 0) {
         return free(ptr), NULL;
-    if ((ptr = realloc(ptr, size)) == NULL)
+    }
+    if ((ptr = realloc(ptr, size)) == NULL) {
         errorf("Out of memory");
+    }
     return ptr;
 }
 
@@ -289,13 +293,15 @@ read_text_file(FILE* stream)
 
     int c;
     while ((c = fgetc(stream)) != EOF) {
-        if (c == '\0')
+        if (c == '\0') {
             errorf("Encountered illegal NUL byte");
+        }
         buf = xalloc(buf, len + 1);
         buf[len++] = (char)c;
     }
-    if (!feof(stream))
+    if (!feof(stream)) {
         errorf("Failed to read entire text file");
+    }
 
     buf = xalloc(buf, len + 1);
     buf[len++] = '\0';
@@ -310,8 +316,9 @@ text_to_lines(char* text)
     lines[count++] = text;
 
     for (; *text; ++text) {
-        if (*text != '\n')
+        if (*text != '\n') {
             continue;
+        }
         *text = '\0';
         lines[count++] = text + 1;
     }
@@ -329,29 +336,37 @@ is_hspace(int c)
 static bool
 is_doc_line(char const* line)
 {
-    if (line == NULL)
+    if (line == NULL) {
         return false;
-    while (is_hspace(*line))
+    }
+    while (is_hspace(*line)) {
         line += 1;
-    if (*line++ != '/')
+    }
+    if (*line++ != '/') {
         return false;
-    if (*line++ != '/')
+    }
+    if (*line++ != '/') {
         return false;
-    if (*line++ != '!')
+    }
+    if (*line++ != '!') {
         return false;
+    }
     return true;
 }
 
 static char const*
 doc_content_start(char const* cp)
 {
-    while (is_hspace(*cp))
-        cp += 1; // Whitespace before '//!'
-    if (cp[0] != '/' || cp[1] != '/' || cp[2] != '!')
+    while (is_hspace(*cp)) { // Whitespace before '//!'
+        cp += 1;
+    }
+    if (cp[0] != '/' || cp[1] != '/' || cp[2] != '!') {
         errorf("[%s] Received non-doc-line", __func__);
+    }
     cp += 3; // Length of "//!"
-    while (is_hspace(*cp))
+    while (is_hspace(*cp)) {
         cp += 1; // Whitespace after '//!'
+    }
     return cp;
 }
 
@@ -398,16 +413,18 @@ parse_struct_source(char*** linepp)
     bool parsed = false; // Are we finished parsing the source?
     int brackets = 0; // Number of '{' minus number of '}'.
     for (; !parsed; *linepp += 1) {
-        if (*linepp == NULL)
+        if (*linepp == NULL) {
             errorf("Unexpected end-of-file");
+        }
         lines = xalloc(lines, (count + 1) * sizeof(char*));
         lines[count++] = **linepp;
 
         for (char* cp = **linepp; *cp != '\0'; ++cp) {
             brackets += *cp == '{';
             brackets -= *cp == '}';
-            if (brackets == 0 && *cp == ';')
+            if (brackets == 0 && *cp == ';') {
                 parsed = true;
+            }
         }
     }
 
@@ -424,8 +441,9 @@ parse_function_source(char*** linepp)
 
     bool parsed = false;
     for (; !parsed; *linepp += 1) {
-        if (*linepp == NULL)
+        if (*linepp == NULL) {
             errorf("Unexpected end-of-file");
+        }
         lines = xalloc(lines, (count + 1) * sizeof(char*));
         lines[count++] = **linepp;
 
@@ -439,8 +457,9 @@ parse_function_source(char*** linepp)
                 lines = xalloc(lines, (count + 1) * sizeof(char*));
                 lines[count++] = "/* function definition... */";
             }
-            if (*cp == '{' || *cp == ';')
+            if (*cp == '{' || *cp == ';') {
                 parsed = true;
+            }
         }
     }
 
@@ -457,8 +476,9 @@ parse_macro_source(char*** linepp)
 
     bool parsed = false;
     for (; !parsed; *linepp += 1) {
-        if (*linepp == NULL)
+        if (*linepp == NULL) {
             errorf("Unexpected end-of-file");
+        }
         lines = xalloc(lines, (count + 1) * sizeof(char*));
         lines[count++] = **linepp;
         parsed = (**linepp)[strlen(**linepp) - 1] != '\\';
@@ -497,10 +517,12 @@ parse_doc(char* const* lines_begin, char*** linepp)
             d.sections[d.section_count++] = parse_section(lines_begin, cur);
         }
     }
-    else if (DOC_IS("function"))
+    else if (DOC_IS("function")) {
         d.source = parse_function_source(linepp);
-    else if (DOC_IS("macro"))
+    }
+    else if (DOC_IS("macro")) {
         d.source = parse_macro_source(linepp);
+    }
 #undef DOC_IS
 
     return d;
@@ -509,13 +531,15 @@ parse_doc(char* const* lines_begin, char*** linepp)
 static void
 print_doc(struct doc const d)
 {
-    for (size_t i = 0; i < d.section_count; ++i)
+    for (size_t i = 0; i < d.section_count; ++i) {
         print_section(d.sections[i]);
+    }
     if (d.source != NULL) {
         puts("<pre><code>");
         char** ln = d.source;
-        while (*ln != NULL)
+        while (*ln != NULL) {
             puts(*ln++);
+        }
         puts("</code></pre>");
     }
     puts("<hr>");
@@ -526,40 +550,48 @@ parse_section(char* const* lines_begin, char*** linepp)
 {
     struct section s = {0};
     char const* cp = doc_content_start(**linepp);
-    if (*cp++ != '@')
+    if (*cp++ != '@') {
         errorf(
             "[line %d] Doc-section must begin with @<TAG>",
             LINENO(lines_begin, *linepp));
-    if (is_hspace(*cp) || *cp == '\0')
+    }
+    if (is_hspace(*cp) || *cp == '\0') {
         errorf("[line %d] Empty doc-comment tag", LINENO(lines_begin, *linepp));
+    }
 
     // TAG
     s.tag_start = cp;
-    while (!is_hspace(*cp) && (*cp != '\0'))
+    while (!is_hspace(*cp) && (*cp != '\0')) {
         cp += 1;
+    }
     s.tag_len = (int)(cp - s.tag_start);
 
-    while (is_hspace(*cp))
+    while (is_hspace(*cp)) {
         cp += 1;
+    }
 
     // NAME
     s.name_start = cp;
-    while (!is_hspace(*cp) && (*cp != '\0'))
+    while (!is_hspace(*cp) && (*cp != '\0')) {
         cp += 1;
+    }
     s.name_len = (int)(cp - s.name_start);
 
-    while (is_hspace(*cp))
+    while (is_hspace(*cp)) {
         cp += 1;
-    if (*cp != '\0')
+    }
+    if (*cp != '\0') {
         errorf(
             "[line %d] Extra character(s) after tag line <NAME>",
             LINENO(lines_begin, *linepp));
+    }
 
     // TEXT
     *linepp += 1;
     s.text_start = *linepp;
-    while (is_doc_line(**linepp) && *doc_content_start(**linepp) != '@')
+    while (is_doc_line(**linepp) && *doc_content_start(**linepp) != '@') {
         *linepp += 1;
+    }
     s.text_len = (int)(*linepp - s.text_start);
 
     return s;
